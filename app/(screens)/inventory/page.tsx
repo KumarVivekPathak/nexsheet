52c2a73300f0041d81a23ff890940af63e6c0846
KumarVivekPathak <coolvivekpathak2004@gmail.com>
2026-03-17 12:58:00 +0530

    inventory page created

---
+++ app/(screens)/inventory/page.tsx
@@ -0,0 +1,10 @@
+const InventoryPage = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold text-gold">Coming Soon....</h1>
+        </div>
+    );
+};
+
+
+export default InventoryPage;
